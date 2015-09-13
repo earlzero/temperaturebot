9d22debbea42f06e122cbd27f79d94d3167a5fe4
Nikita Sapozhnikov <earlzero@gmail.com>
2015-09-13 21:57:38 +0300

    Switch to webhook

--- src/main/java/org/home/temperature/server/Main.java
+++ src/main/java/org/home/temperature/server/Main.java
@@ -1,45 +1,48 @@
 package org.home.temperature.server;
 
-import org.glassfish.grizzly.http.server.HttpHandler;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
-import org.home.temperature.server.telegram.TelegramBot;
+import org.home.temperature.server.telegram.rest.UpdateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
 
 public class Main {
 
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+	public static final String BASE_URL = "https://api.telegram.org/bot";
+	private static final String SET_WEBHOOK_METHOD = "setWebhook";
+
+	private static void setWebHook(String token, String url) {
+		Client client = ClientBuilder.newClient();
+		Invocation.Builder ib = client.target(String.join("", BASE_URL, token)).path(SET_WEBHOOK_METHOD)
+				.request(MediaType.APPLICATION_JSON_TYPE);
+		String request = String.format("{\"url\":\"%s\"", url);
+		Response response = ib.post(Entity.entity(request, MediaType.APPLICATION_JSON));
+		logger.info(String.format("Registration %s", response.getStatus()));
+	}
+
 	public static void main(String... args) throws Exception {
 
-		TelegramBot telegramBot = new TelegramBot(System.getProperty("telegram.token"));
-		
-		new Thread(telegramBot).start();
-
-		HttpServer server = HttpServer.createSimpleServer(null, Integer.valueOf(System.getProperty("port")));
-		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
-
-			@Override
-			public void service(Request request, Response response) throws Exception {
-
-				try {
-					float temperature = Float.parseFloat(request.getParameter("temp0"));
-					String result = "ok";
-					response.setContentType("text/plain");
-					response.setContentLength(result.length());
-					response.getWriter().write(result);
-					telegramBot.setTemperature(temperature);
-				} catch (NumberFormatException e) {
-					logger.error("Can't parse temp0", e);
-				}
-
-			}
-		});
+		setWebHook(System.getProperty("telegram.token"), System.getProperty("webhook.url"));
+		ResourceConfig rc = new PackagesResourceConfig(UpdateService.class.getPackage().getName());
+		HttpServer server = GrizzlyServerFactory
+				.createHttpServer(String.format("http://0.0.0.0:%s", System.getProperty("port")), rc);
 		server.start();
+
 		while (!Thread.currentThread().isInterrupted()) {
 			Thread.sleep(1000);
 		}
+
 	}
 }
---
+++ src/main/java/org/home/temperature/server/telegram/rest/UpdateService.java
@@ -0,0 +1,64 @@
+package org.home.temperature.server.telegram.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
+import org.home.temperature.server.Main;
+import org.home.temperature.server.telegram.Message;
+import org.home.temperature.server.telegram.OutgoingMessage;
+import org.home.temperature.server.telegram.Update;
+
+@Path("/")
+public class UpdateService {
+
+	private volatile float temperature;
+
+	private WebTarget webTarget;
+
+	private static final String SEND_MESSAGE_METHOD = "sendMessage";
+
+	private WebTarget createTarget(String baseUrl, String token) {
+		ClientConfig clientConfig = new ClientConfig(new MoxyJsonFeature());
+		Client client = ClientBuilder.newClient(clientConfig);
+		String url = String.join("", baseUrl, token);
+		return client.target(url);
+	}
+
+	public UpdateService() {
+		webTarget = createTarget(Main.BASE_URL, System.getProperty("telegram.token")).path(SEND_MESSAGE_METHOD);
+	}
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(Update update) {
+		Message msg = update.getMessage();
+		if (msg.getChat().getId() == 130318030) {
+			OutgoingMessage outMsg = new OutgoingMessage();
+			outMsg.setChat_id(msg.getChat().getId());
+			outMsg.setText(String.format("Temperature is %.2f", temperature));
+			Invocation.Builder sendBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
+			sendBuilder.accept(MediaType.APPLICATION_JSON).post(Entity.entity(outMsg, MediaType.APPLICATION_JSON),
+					String.class);
+		}
+		return Response.status(201).entity("ok").build();
+	}
+
+	@GET
+	@Path("/add")
+	public Response addTemperature(@QueryParam("temp0") float temperature) {
+		return Response.status(200).entity("ok").build();
+	}
+}
